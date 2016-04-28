12ea645546710a8f687c2a8eb4db62826fad8d2c
zachstecher <malleablemedia@gmail.com>
2016-04-28 12:41:06 -0400

    Added ECB mode
    ECB (Electronic Code Book) mode handles plaintext padding by determining the number of bytes required to fill a block and padding the end with it. The method also performs encryption as many blocks as it takes to encrypt the entire plaintext message.

--- driver.java
+++ driver.java
@@ -6,7 +6,7 @@
  * Assignment: Final Project
  * 
  * This file passes the key and plaintext from its .txt file 
- * into the aes class to generate the round keys and
+ * into the AES class to generate the round keys and
  * encrypt the text.
  *
  */
@@ -20,11 +20,56 @@ public class driver {
     String theKey = input.nextLine();
     cipher.aesRoundKeys(theKey);
     String plainText = input.nextLine();
-    cipher.aesEncrypt(plainText, cipher.W);
+    ECB(plainText, cipher.W);
     
     String theKey = "2b7e151628aed2a6abf7158809cf4f3c";
     String plainText = "3ad77bb40d7a3660a89ecaf32466ef97";
     decrypt.aesRoundKeys(theKey);
     decrypt.aesDecrypt(plainText, decrypt.W);
+    
+    input.close();
+  }
+  public static String ECB(String s, String k){
+	AES cipher = new AES();
+    int mSize = s.length();
+    int padSize = 0;
+    int blocks = 0;
+    
+    if (mSize < 32){
+      padSize = ((32 - mSize) / 2);
+      for (int i = 0; i < padSize; i++){
+        if (Integer.toHexString(padSize).length() < 2){
+          s = s + ("0" + Integer.toHexString(padSize));
+        }
+        else{
+          s = s + Integer.toHexString(padSize);	
+        }
+      }
+      
+      cipher.aesEncrypt(s, cipher.W);
+    }
+    
+    if (mSize > 32){
+      if (mSize % 32 != 0){
+        padSize = (32 - (mSize % 32)) / 2;
+        for (int i = 0; i < padSize; i++){
+          if (Integer.toHexString(padSize).length() < 2){
+            s = s + ("0" + Integer.toHexString(padSize));
+          }
+          else{
+              s = s + Integer.toHexString(padSize);	
+          }
+        }
+      }
+    }
+    
+    blocks = (mSize / 32);
+    for (int i = 0; i < blocks; i++){
+    	cipher.aesRoundKeys(k);
+        cipher.aesEncrypt(s.substring((i * 32), (32*(i+1))), cipher.W);
+        System.out.println("");
+    }
+    
+    return s;
   }
 }
